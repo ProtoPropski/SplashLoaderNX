66f41df4b606ccf401c44e258346ddd25521e7af
Proto Propski <39485155+ProtoPropski@users.noreply.github.com>
2018-05-21 07:19:46 -0700

    Add Small Features
    Added Main Menu
    Added Delete Function
    Fixed Some Bugs

--- Splash Loader.bat
+++ Splash Loader.bat
@@ -2,7 +2,7 @@
 	cls
 	@echo off
 	color 3
-	title Splash Loader v1
+	title Splash Loader v1.2
 	
 		Echo    _____       __           __         __                    __         
 		Echo   / ___/____  / /___ ______/ /_       / /   ____  ____ _____/ /__  _____
@@ -11,35 +11,125 @@
 		Echo /____/ .___/_/\__,_/____/_/ /_/    /_____/\____/\__,_/\__,_/\___/_/     
 		Echo     /_/                                           by Proto Propski                                  
 		Echo.
- 
+		Echo ===========================================================================
+		Echo.
+		
+		Echo.                                                                                                                                                                                                  
+		Echo [1] -Splash Selection Screen                                                                                                                                                                      
+		Echo [2] -Delete A Splash File
+		Echo.
+		Echo [0] -Exit
+		Echo.
+
+			set /P Choice=Choice: 
+
+				if %Choice% == 0 goto 0
+				if %Choice% == 1 goto SplashSelect
+				if %Choice% == 2 goto DeleteSplash
+					
+					Echo.
+					Echo (Error 1) Type a valid value.
+
+						ping localhost -n 3 >nul
+							goto HomeMenu
+
+:SplashSelect
+	cls
+	@echo off
+	color 3
+	title Splash Selection Screen
+	
+		Echo    _____       __           __         __                    __         
+		Echo   / ___/____  / /___ ______/ /_       / /   ____  ____ _____/ /__  _____
+		Echo   \__ \/ __ \/ / __ `/ ___/ __ \     / /   / __ \/ __ `/ __  / _ \/ ___/
+		Echo  ___/ / /_/ / / /_/ (__  ) / / /    / /___/ /_/ / /_/ / /_/ /  __/ /    
+		Echo /____/ .___/_/\__,_/____/_/ /_/    /_____/\____/\__,_/\__,_/\___/_/     
+		Echo     /_/                                           by Proto Propski                                  
+		Echo.
+		Echo ===========================================================================
+		Echo.
 		Echo  !! IMPORTANT !!
 
 		Echo.
-		Echo Place Your "Splash#.bin" File in the Same Folder as the "Splash Loader" file
+		Echo Place Your Splash#.bin files in the same Folder as this program on the Root 
+		Echo of your SD Card (Ex. smdc:/Splash Loader/Splash#.bin)
+		Echo.
 		Echo Then Type the # of your Splash File Below.
 		Echo.
 		Echo.
 		Echo (# = Number)
 		Echo.
+		Echo To Return to the Main Menu type M / Menu
 
-			set /P Number=Number:   
+			set /P Number=Number: 
 
-				if %Number% == Exit goto 0
-				if %Number% == exit goto 0
+				if %Number% == M goto HomeMenu
+				if %Number% == m goto HomeMenu
+				if %Number% == Menu goto HomeMenu
+				if %Number% == menu goto HomeMenu
 				if %Number% == %Number% goto Transfer
 
-
-					Echo Error 1
 					Echo.
-					Echo Type a correct number value.
+					Echo (Error 1) Type a valid value.
 
-						ping localhost -n 7 >nul
-							goto :HomeMenu
+						ping localhost -n 3 >nul
+							goto SplashSelect
 
 
 :0
 	
+	exit
 :: This Area is For Exiting
+
+:DeleteSplash
+	cls
+	Title Splash Loader v1.2
+	@Echo Off
+	
+		Echo    _____       __           __         __                    __         
+		Echo   / ___/____  / /___ ______/ /_       / /   ____  ____ _____/ /__  _____
+		Echo   \__ \/ __ \/ / __ `/ ___/ __ \     / /   / __ \/ __ `/ __  / _ \/ ___/
+		Echo  ___/ / /_/ / / /_/ (__  ) / / /    / /___/ /_/ / /_/ / /_/ /  __/ /    
+		Echo /____/ .___/_/\__,_/____/_/ /_/    /_____/\____/\__,_/\__,_/\___/_/     
+		Echo     /_/                                           by Proto Propski                                  
+		Echo.
+		Echo ===========================================================================	
+		Echo.
+
+		Echo Choose the Splash#.bin File you Wish to Delete
+		Echo.
+		Echo.
+		Echo To Return to the Main Menu type M / Menu
+		
+			set /P ChoiceD=Choice: 
+
+				if %ChoiceD% == M goto HomeMenu
+				if %ChoiceD% == m goto HomeMenu
+				if %ChoiceD% == Menu goto HomeMenu
+				if %ChoiceD% == menu goto HomeMenu
+				if %ChoiceD% ==  %ChoiceD% goto Delete
+
+					Echo.
+					Echo (Error 1) Type a valid value.
+
+						ping localhost -n 3 >nul
+							goto DeleteSplash
+		
+:Delete
+		
+		Echo.
+		Echo Deleting Chosen Splash#.bin File.
+		Echo.
+		
+			del Splash%ChoiceD%.bin
+				ping localhost -n 1 >nul
+		Echo.
+		Echo Delete Successful, Press Any Button to Return to the Main Menu
+		Echo.
+		
+			pause >nul
+		
+		goto HomeMenu
 	
 :Transfer
 	cls
@@ -53,7 +143,7 @@
 		Echo /____/ .___/_/\__,_/____/_/ /_/    /_____/\____/\__,_/\__,_/\___/_/     
 		Echo     /_/                                           by Proto Propski                                  
 		Echo.
-	
+		Echo ===========================================================================	
 		Echo.
 		Echo Transferring Your Splash
 		Echo.
@@ -85,5 +175,5 @@
 		
 			pause >nul
 
-			goto :HomeMenu
+			goto HomeMenu
 			
